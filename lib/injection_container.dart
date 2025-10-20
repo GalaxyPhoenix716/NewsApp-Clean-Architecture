4d7efd629a0eb7b6495a6b4c1827ef7f8bd4ef54
GalaxyPhoenix <galaxyphoenix716@gmail.com>
2025-10-20 09:29:09 +0530

    created get articles bloc and initialized injection dependencies

---
+++ lib/injection_container.dart
@@ -0,0 +1,25 @@
+import 'package:clean_arch/features/daily_news/data/data_sources/remote/news_api_service.dart';
+import 'package:clean_arch/features/daily_news/data/repositories/article_reppsitory_impl.dart';
+import 'package:clean_arch/features/daily_news/domain/repositories/article_repository.dart';
+import 'package:clean_arch/features/daily_news/domain/usecases/get_article.dart';
+import 'package:clean_arch/features/daily_news/presentation/bloc/article/remote/remote_article_bloc.dart';
+import 'package:dio/dio.dart';
+import 'package:get_it/get_it.dart';
+
+final sl = GetIt.instance;
+
+Future<void> initializeDependencies() async {
+  //Dio
+  sl.registerSingleton<Dio>(Dio());
+
+  //Dependencies
+  sl.registerSingleton<NewsApiService>(NewsApiService(sl()));
+
+  sl.registerSingleton<ArticleRepository>(ArticleReppsitoryImpl(sl()));
+
+  sl.registerSingleton<GetArticleUseCase>(GetArticleUseCase(sl()));
+
+  //Bloc
+  sl.registerFactory<RemoteArticleBloc>(() => RemoteArticleBloc(sl()));
+
+}
